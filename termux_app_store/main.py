2ffce847cc62704f434e61a6f4da22b153be79e4
djunekz <gab288.gab288@passinbox.com>
2026-02-27 00:51:07 +0700

    Upload file master main.py

---
+++ termux_app_store/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+from termux_app_store import run_tui
+from termux_app_store_cli import run_cli
+
+def main():
+    if len(sys.argv) > 1:
+        run_cli()
+    else:
+        run_tui()
+
+if __name__ == "__main__":
+    main()
