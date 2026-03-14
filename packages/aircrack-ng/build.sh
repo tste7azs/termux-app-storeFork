4de34656d32ac1ceb07f421c10991dcca7424f1d
djunekz <gab288.gab288@passinbox.com>
2026-03-15 02:19:46 +0700

    Update packages

--- packages/aircrack-ng/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/pitube08642/aircrack-ng-for-termux
-TERMUX_PKG_DESCRIPTION="aircrack-ng for termux package"
-TERMUX_PKG_LICENSE="UNKNOWN"
-TERMUX_PKG_MAINTAINER="@termux-app-store"
-TERMUX_PKG_VERSION=1.7
-TERMUX_PKG_SRCURL=https://github.com/pitube08642/aircrack-ng-for-termux/releases/download/Aircrack-ng_termux/aircrack-ng_3_1.7_aarch64.deb
-TERMUX_PKG_SHA256=809dcc9f0405e170bd1ef8e63d4f7411a49167cee2eb316842e0dd13e3073aa6
