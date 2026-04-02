3baae610d4d5782d22b47d131c1b4e447b8e82e1
djunekz <gab288.gab288@passinbox.com>
2026-04-03 05:14:41 +0700

    Updated packages

--- packages/zx/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/google/zx
-TERMUX_PKG_DESCRIPTION="A tool for writing better scripts"
-TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_MAINTAINER="@termux-app-store"
-TERMUX_PKG_VERSION=8.8.5
-TERMUX_PKG_SRCURL=https://api.github.com/repos/google/zx/tarball/${TERMUX_PKG_VERSION}
-TERMUX_PKG_SHA256=7d1594daa43df8bf2b49738db628f6d66646e494f3151750c373da012d189976
