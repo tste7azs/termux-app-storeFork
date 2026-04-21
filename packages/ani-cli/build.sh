ce566eeed5820fd538ebde8b4c22ef40b1f4bac0
djunekz <gab288.gab288@passinbox.com>
2026-04-21 08:20:52 +0700

    Update dependencies ani-cli

--- packages/ani-cli/build.sh
+++ packages/ani-cli/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux-app-store"
 TERMUX_PKG_VERSION=4.11
 TERMUX_PKG_SRCURL=https://github.com/pystardust/ani-cli/releases/download/v${TERMUX_PKG_VERSION}/ani-cli
 TERMUX_PKG_SHA256=1f35c47b0c6d924261d096663d16b03d489f488fcc38c74e4fe87b45a330b7cf
-TERMUX_PKG_DEPENDS="nodejs"
+TERMUX_PKG_DEPENDS="nodejs, fzf"
 
 termux_step_make_install() {
     npm install -g --prefix "$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
